0406e2d9fa34d66f3af0ea795664d72b7e75592c
kafkaaaa <csky222@naver.com>
2020-02-11 18:24:44 +0900

    Update Sum_15596.java

--- Sum_15596.java
+++ Sum_15596.java
@@ -1,9 +1,11 @@
-package stepbystep;
-
-public class Sum_15596 {
-
-	public static void main(String[] args) {
-
+public class Test {
+	long sum(int[] a) {
+		long ans = 0;
+		
+		/* 정수 n개가 주어졌을 때, n개의 합을 구하는 함수 */
+		for	(int i=0; i<a.length; i++) {
+			ans += a[i];
+		}
+		return ans;
 	}
-
 }
