0581e98f36999b0edf5a500d4d5f8d521fbb1088
kafkaaaa <csky222@naver.com>
2020-05-16 00:31:07 +0900

    day35
    정렬

---
+++ Statistics_2108.java
@@ -0,0 +1,33 @@
+package stepbystep;
+
+import java.util.Scanner;
+
+public class Statistics_2108 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();		// 수의 개수 n
+		
+
+	}
+	
+	static int avg(int[] num) {
+		int sum = 0;
+		for (int i=0; i<num.length; i++) {
+			sum += num[i];
+		}
+		return Math.round(sum / num.length);
+	}
+	
+	static int mid(int[] num) {
+		
+	}
+	
+	static int min(int[] num) {
+		
+	}
+	
+	static 
+
+}
