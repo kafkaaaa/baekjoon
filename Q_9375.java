ce366c6261b744c3faf50fc27e268d93fa0daf27
kafkaaaa <csky222@naver.com>
2021-01-28 21:30:58 +0900

    day73
    // 정수론 및 조합론 - 팩토리얼 0의 개수
    // https://www.acmicpc.net/problem/9375

---
+++ Q_9375.java
@@ -0,0 +1,30 @@
+package stepbystep;
+
+// 정수론 및 조합론 - 팩토리얼 0의 개수
+// https://www.acmicpc.net/problem/9375
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_9375 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		// Solve
+		// 뒤에 0이 붙으려면 2x5가 있어야 한다.
+		// 2와 5가 세트로 곱해진 개수가 팩토리얼 0의 개수가 된다.
+		// 5의 개수가 2의 개수보다 항상 적으므로 5의 개수만 세도 된다.
+		int cnt = 0;
+		
+		while (n >= 5) {
+			cnt += n/5;
+			n /= 5;
+		}
+		System.out.println(cnt);
+	}
+
+}
